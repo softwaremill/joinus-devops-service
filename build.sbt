4f302a9bf000fcb1840d450dba825de1d701086f
adamw <adam@warski.org>
2015-10-29 14:58:35 +0100

    Server impl

---
+++ build.sbt
@@ -0,0 +1,19 @@
+import sbt._
+import Keys._
+
+val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.12"
+val logBackClassic = "ch.qos.logback" % "logback-classic" % "1.1.3"
+val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0"
+val json4s = "org.json4s" %% "json4s-native" % "3.2.10"
+val akkaHttp = "com.typesafe.akka" %% "akka-http-experimental" % "1.0"
+val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % "2.3.12"
+
+name := "joinus-devops-service"
+
+organization := "com.softwaremill"
+
+version := "0.0.1-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= List(slf4jApi, logBackClassic, scalaLogging, json4s, akkaHttp, akkaSlf4j)
---
+++ src/main/scala/com/softwaremill/Main.scala
@@ -0,0 +1,52 @@
+package com.softwaremill
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+import com.typesafe.scalalogging.StrictLogging
+
+import scala.util.{Failure, Success}
+
+object Main extends App with StrictLogging {
+  implicit val as = ActorSystem()
+  import as.dispatcher
+  implicit val mat = ActorMaterializer()
+
+  var isOk = true
+
+  val routes = get {
+    path("status") {
+      if (isOk) {
+        logger.info("Status ok")
+        complete("""{"status": "ok"}""")
+      } else {
+        logger.error("Error")
+        complete(500, "ERROR")
+      }
+    }
+  } ~ post {
+    path("dowork") {
+      parameter("magicnumber".as[Int]) { mn =>
+        if (mn == 42) {
+          sys.exit(1)
+        } else {
+          if (mn % 2 == 0) {
+            isOk = true
+            complete("done")
+          } else {
+            isOk = false
+            complete("try again")
+          }
+        }
+      }
+    }
+  }
+
+  Http().bindAndHandle(routes, "0.0.0.0", 8081).onComplete {
+    case Success(_) => logger.info("Server started")
+    case Failure(f) =>
+      logger.error("Cannot start server", f)
+      as.shutdown()
+  }
+}
